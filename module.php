0028418c1246c2500184e52bb68656858754ada0
Jose Bernal <jbernal.web.dev@gmail.com>
2020-11-10 18:16:29 -0800

    Consolidate tests.

--- module.php
+++ module.php
@@ -1,22 +1,24 @@
 <?php 
 
-class TestModule extends Module{
+class TestModule extends Module {
 
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
     }
 
-    public function testFunctionOne(){
+		
+    public function testFunctionOne() {
         return "Hello from the first test function in the module.php file of the test module";
     }
 
-    public function testFunctionTwo($testParam){
+    public function testFunctionTwo($testParam) {
         $testParam = "Hello from the second test function in the module.php file of the test module";
+        
+        
         return $testParam;
     }
 
-    public function testFunctionThree(){
-        $tc = New TestClass();
-        return $tc->testMethodOne();
+    public function testFunctionThree() {
+        throw new Exception("Error generated from the Test module.");
     }
 }
