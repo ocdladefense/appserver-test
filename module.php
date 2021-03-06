2052eaf960b4655880e5ffe60cfc537971c4f873
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-03-05 17:40:39 -0800

    upload-attachment

--- module.php
+++ module.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+
+use Http\Http as Http;
+use File\FileList as FileList;
+use File\File as File;
 
 class TestModule extends Module {
 
@@ -8,150 +12,148 @@ class TestModule extends Module {
 
 		
 		
-		public function userProfile() {
+	public function userProfile() {
 
-			$title = "<h2>My profile</h2>";
-			$content = "<a href='/user/logout'>Logout of OCDLA App</a><p>&nbsp;</p>";
-			
-			// This session will be a SimpleSAML session.
-			return $title."<pre>" . print_r($_SESSION, true) . "</pre>";
-		}
-
-
-		public function userSettings() {
+		$title = "<h2>My profile</h2>";
+		$content = "<a href='/user/logout'>Logout of OCDLA App</a><p>&nbsp;</p>";
 		
-			return "<h2>OCDLA App Settings</h2>";
-		}
-		
-		
-		public function userDocuments() {
-		
-			$docs = array(
-				"dnb" => array(
-					"id" => "dnb",
-					"title" => "DUII Notebook",
-					"chapters" => 18,
-					"published" => "Jan. 1, 2020"
-				),
+		// This session will be a SimpleSAML session.
+		return $title."<pre>" . print_r($_SESSION, true) . "</pre>";
+	}
 
 
-				"fsm" => array(
-					"id" => "fsm",
-					"title" => "Felony Sentencing in Oregon",
-					"chapters" => 10,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"im" => array(
-					"id" => "im",
-					"title" => "Investigators Manual",
-					"chapters" => 18,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"ss" => array(
-					"id" => "ss",
-					"title" => "Search & Seizure",
-					"chapters" => 10,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"vet" => array(
-					"id" => "vet",
-					"title" => "Veterans Manual",
-					"chapters" => 15,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"sex-crimes" => array(
-					"id" => "sex-crimes",
-					"title" => "Defending Sex Cases",
-					"chapters" => 19,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"mh" => array(
-					"id" => "mh",
-					"title" => "Mental Health Manual",
-					"chapters" => 8,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"post-judgement" => array(
-					"id" => "post-judgement",
-					"title" => "Post-Judgment Manual",
-					"chapters" => 11,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"scientific-evidence" => array(
-					"id" => "scientific-evidence",
-					"title" => "Scientific Evidence",
-					"chapters" => 7,
-					"published" => "Jan. 1, 2020"
-				),
-				
-				"trial-notebook" => array(
-					"id" => "trial-notebook",
-					"title" => "Trial Notebook",
-					"chapters" => 18,
-					"published" => "Jan. 1, 2020"
-				)
-			);
-			
-			
-
-			
-			$files = array_map(function($doc) {
-				$id = "sex-crimes";
-				$title = $doc["title"];
-				$chapters = $doc["chapters"];
-				$published = "Jan. 1, 2020";
-				$url = "/content/images/document.svg";
-
-				
-				return "<div class='doc'><a href='/books-online/{$id}' title='{$title}'><img style='width: 30%;' src='{$url}' /></a><div class='doc-info'><span class='doc-title'>{$title}</span><span class='doc-chapters'>{$chapters} chapters</span><span class='doc-published'>{$published}</span>    </div></div>";
-			}, $docs);
-			
-			return join("\n",$files);
-		}
-
-
-		public function loadChapter1($bonId) {
-		
-		
-		
-		
-			return file_get_contents("/var/www/webapp/appserver/content/modules/bon/sample.html");
-		}
-
+	public function userSettings() {
 	
+		return "<h2>OCDLA App Settings</h2>";
+	}
+	
+	
+	public function userDocuments() {
+	
+		$docs = array(
+			"dnb" => array(
+				"id" => "dnb",
+				"title" => "DUII Notebook",
+				"chapters" => 18,
+				"published" => "Jan. 1, 2020"
+			),
 
-		public function login() {
 
+			"fsm" => array(
+				"id" => "fsm",
+				"title" => "Felony Sentencing in Oregon",
+				"chapters" => 10,
+				"published" => "Jan. 1, 2020"
+			),
 			
-				$as = new \SimpleSAML\Auth\Simple('default-sp');
-
-				$as->requireAuth();
-
-				$attributes = $as->getAttributes();
-				// print_r($attributes);
+			"im" => array(
+				"id" => "im",
+				"title" => "Investigators Manual",
+				"chapters" => 18,
+				"published" => "Jan. 1, 2020"
+			),
 			
-				// This session will be a SimpleSAML session.
-				// print_r($_SESSION);
+			"ss" => array(
+				"id" => "ss",
+				"title" => "Search & Seizure",
+				"chapters" => 10,
+				"published" => "Jan. 1, 2020"
+			),
 			
-				// This session will be a PHP session.
-				// cleanup the SimpleSAML session; also restores the previous session.
-				$session = \SimpleSAML\Session::getSessionFromRequest();
-				$session->cleanup();
+			"vet" => array(
+				"id" => "vet",
+				"title" => "Veterans Manual",
+				"chapters" => 15,
+				"published" => "Jan. 1, 2020"
+			),
 			
-				User::add_session_data($attributes);
-				
-				header("Location: /home");
-				exit;
-		}
+			"sex-crimes" => array(
+				"id" => "sex-crimes",
+				"title" => "Defending Sex Cases",
+				"chapters" => 19,
+				"published" => "Jan. 1, 2020"
+			),
+			
+			"mh" => array(
+				"id" => "mh",
+				"title" => "Mental Health Manual",
+				"chapters" => 8,
+				"published" => "Jan. 1, 2020"
+			),
+			
+			"post-judgement" => array(
+				"id" => "post-judgement",
+				"title" => "Post-Judgment Manual",
+				"chapters" => 11,
+				"published" => "Jan. 1, 2020"
+			),
+			
+			"scientific-evidence" => array(
+				"id" => "scientific-evidence",
+				"title" => "Scientific Evidence",
+				"chapters" => 7,
+				"published" => "Jan. 1, 2020"
+			),
+			
+			"trial-notebook" => array(
+				"id" => "trial-notebook",
+				"title" => "Trial Notebook",
+				"chapters" => 18,
+				"published" => "Jan. 1, 2020"
+			)
+		);
+		
 		
 
+		
+		$files = array_map(function($doc) {
+			$id = "sex-crimes";
+			$title = $doc["title"];
+			$chapters = $doc["chapters"];
+			$published = "Jan. 1, 2020";
+			$url = "/content/images/document.svg";
+
+			
+			return "<div class='doc'><a href='/books-online/{$id}' title='{$title}'><img style='width: 30%;' src='{$url}' /></a><div class='doc-info'><span class='doc-title'>{$title}</span><span class='doc-chapters'>{$chapters} chapters</span><span class='doc-published'>{$published}</span>    </div></div>";
+		}, $docs);
+		
+		return join("\n",$files);
+	}
+
+
+	public function loadChapter1($bonId) {
+	
+	
+	
+	
+		return file_get_contents("/var/www/webapp/appserver/content/modules/bon/sample.html");
+	}
+
+
+
+	public function login() {
+		
+		$as = new \SimpleSAML\Auth\Simple('default-sp');
+
+		$as->requireAuth();
+
+		$attributes = $as->getAttributes();
+		// print_r($attributes);
+	
+		// This session will be a SimpleSAML session.
+		// print_r($_SESSION);
+	
+		// This session will be a PHP session.
+		// cleanup the SimpleSAML session; also restores the previous session.
+		$session = \SimpleSAML\Session::getSessionFromRequest();
+		$session->cleanup();
+	
+		User::add_session_data($attributes);
+		
+		header("Location: /home");
+		exit;
+	}
+		
 
 		
     public function testFunctionOne() {
@@ -178,5 +180,77 @@ class TestModule extends Module {
 
     public function testFunctionThree() {
         throw new Exception("Error generated from the Test module.");
-    }
+	}
+	
+
+	// Connect to salesforce using a connected app
+	public function connectToSalesforce(){
+
+		$apiRequest = $this->loadForceApi();
+
+		//$soql = "SELECT Id, FirstName, LastName FROM Contact WHERE FirstName = 'Trevor'";
+
+		//$apiResponse = $apiRequest->query($soql);
+
+		// $contact1 = array("FirstName" => "Trev1", "LastName" => "Ueh1");
+
+		// $contact2 = array("FirstName" => "Trev2", "LastName" => "Ueh2");
+
+		// $contact3 = array("FirstName" => "Trev3", "LastName" => "Ueh3");
+
+		// $objects = array($contact1, $contact2, $contact3);
+
+		//$apiResponse = $apiRequest->sendBatch($objects, "Contact");
+
+
+		$product1 = array("Name" => "prod1");
+		
+		$objects = array($product1);
+		
+		$apiResponse = $apiRequest->sendBatch($objects, "Product2");
+
+		var_dump($apiResponse); exit;
+	}
+
+
+	public function callTestAttachmentUpload(){
+
+		$api = $this->loadForceApi("ocdla-sandbox");
+		$parentId = "a1Kj0000000TordEAC";
+
+		$req = $this->getRequest();
+
+		//$fileName = "XXXXX_TEST_TEXT.txt";
+		//$fileName = "XXXXX_TEST_PDF.pdf";
+		$fileName = "XXXXX_TEST_PNG.png";
+		$filePath = getPathToContent() . "/testFiles/{$fileName}";
+
+		
+
+		$list = new FileList();
+		$list->addFile(File::fromPath($filePath));
+
+		$file = new File("HelloWorld.txt");
+		$file->setContent("Hello World");
+		$list->addFile($file);
+		
+		$req->setFiles($list);
+
+		$fileList = $req->getFiles();
+
+		try{
+			
+			$resp = $api->uploadFiles($fileList, $parentId);
+
+		} catch(Exception $e) {
+
+			// Let the user know something went wrong.
+		}
+
+
+		var_dump($resp);
+
+		exit;
+
+	}
 }
